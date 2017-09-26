9b8f3bb77bc4a419660bc84bfee0566b693de932
scotthargroves <shargroves@salesforce.com>
2017-09-27 09:11:30 +1000

    Added GA Tracking

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	
   <meta charset="UTF-8">
   <title>Auto-Acknowledgment Email Designer - Desk.com Toolbox</title>
   
@@ -971,6 +972,17 @@
         });
         
       </script>
+      
+      	<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-10299982-25', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>
 	
 	<!-- FOOTER JS END -->
 
